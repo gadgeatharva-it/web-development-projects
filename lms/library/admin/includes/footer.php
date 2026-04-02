9bef330a2122802af157d6b15f988b3973d307ba
Athu077 <gadgeathu07@gmail.com>
2026-04-02 23:14:55 +0530

    Fix nested git repository issue

---
+++ lms/library/admin/includes/footer.php
@@ -0,0 +1,10 @@
+   <section class="footer-section">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                   &copy; 2024 Online Library Management System |<a href="https://github.com/Athu077" target="_blank"  > Designed by : gadgeatharva07</a> 
+                </div>
+
+            </div>
+        </div>
+    </section>
---
+++ lms/library/includes/footer.php
@@ -0,0 +1,15 @@
+   <section class="footer-section">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                   &copy; 2024 Online Library Management System |<a href="https://github.com/Athu077" target="_blank"  > Designed by : gadgeatharva07</a> 
+                </div>
+
+            </div>
+        </div>
+    </section>
+    <style>
+        .col-md-12{
+            text-align: center;
+        }
+    </style>
